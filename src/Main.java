c99b7c709a4576afe9193ae98d844186c232a615
a1 <Skif.Olshan@gmail.com>
2022-07-22 14:14:29 +0200

    Done, fixed third time.

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,80 @@
 import java.sql.PreparedStatement;
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
 
-        //Task 1
-     //   int[] weights = new int[] {1, 2, 3};
-     //   float[] weightKg = {1.57f, 7.654f, 9.986f};
-     //   int[] height = {180, 182, 190};
+        System.out.println("Задание 1 и 2");
 
-        //Task 2,3,4
-
-        int[] weights = new int[] {10, 20, 30};
-        for (int i = weights.length - 1; i >= 0; i--) {
-            if (i <= weights.length)
-                if (i == 0) {
-                    System.out.println(weights[i]);
-                }
-                else System.out.print(weights[i] + ",");
+        int[] weights = new int[]{1, 2, 3};
+        for (int i = 0; i < weights.length; i++) {
+            if (i == weights.length - 1) {
+                System.out.print(weights[i]);
+            } else {
+                System.out.print(weights[i] + ", ");
+            }
         }
         System.out.println();
         float[] weightKg = {1.57f, 7.654f, 9.986f};
-        for (int i = weightKg.length - 1; i >= 0; i--) {
-            if (i == 0) {
-                System.out.println(weightKg[i]);
+        for (int i = 0; i < weightKg.length; i++) {
+            if (i == weightKg.length - 1) {
+                System.out.print(weightKg[i]);
+            } else {
+                System.out.print(weightKg[i] + ", ");
             }
-            else System.out.print(weightKg[i] + ",");
         }
         System.out.println();
         int[] height = {180, 182, 190};
-        for (int i = height.length - 1; i >= 0; i--) {
-            if (i == 0){
-                System.out.println(height[i]);
-                break;
+        for (int i = 0; i < height.length; i++) {
+            if (i == height.length - 1) {
+                System.out.print(height[i]);
+            } else {
+                System.out.print(height[i] + ", ");
             }
-            else System.out.print(height[i] + ",");
         }
+        System.out.println();
+        System.out.println("Задание 3");
+
+        for (int i = weights.length - 1; i >= 0; i--) {
+            if (i <= weights.length)
+            if (i == 0) {
+                System.out.print(weights[i]);
+            } else {
+                System.out.print(weights[i] + ", ");
+
+            }
+        }
+        System.out.println();
+        for (int i = weightKg.length - 1; i >= 0; i--) {
+            if (i <= weightKg.length)
+                if (i == 0) {
+                    System.out.print(weightKg[i]);
+                } else {
+                    System.out.print(weightKg[i] + ", ");
+                }
+        }
+        System.out.println();
+        for (int i = height.length - 1; i >= 0 ; i--) {
+            if (i <= height.length)
+                if (i == 0) {
+                    System.out.print(height[i]);
+                } else {
+                    System.out.print(height[i] + ", ");
+                }
+
+        }
+        System.out.println();
+        System.out.println("Задание 4");
+        for (int i = 0; i < weights.length; i++) {
+            if (weights[i] % 2 !=0) {
+                weights[i] = weights[i] + 1;
+            }
+            if (i < weights.length - 1)
+                System.out.print(weights[i] + ", ");
+            else
+                System.out.println(weights[i]);
+
+        }
+
     }
 }
